b29f4255272b9167361d91ba7e44f9a3548b0c29
David suarez <dav.sua.pas@gmail.com>
2017-04-27 14:33:22 +0200

    init customer school

---
+++ CustomerSchool/src/main/java/com/elipcero/customerschool/CustomerSchoolApplication.java
@@ -0,0 +1,12 @@
+package com.elipcero.customerschool;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CustomerSchoolApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CustomerSchoolApplication.class, args);
+	}
+}
