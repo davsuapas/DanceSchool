47b39ecb8660be97e850e9798613040891b8db09
David suarez <dav.sua.pas@gmail.com>
2017-05-04 15:27:04 +0200

    Configure customer school like eureka client

--- CustomerSchool/src/main/java/com/elipcero/customerschool/CustomerSchoolApplication.java
+++ CustomerSchool/src/main/java/com/elipcero/customerschool/CustomerSchoolApplication.java
@@ -2,11 +2,13 @@ package com.elipcero.customerschool;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @SpringBootApplication
+@EnableEurekaClient
 public class CustomerSchoolApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(CustomerSchoolApplication.class, args);
+		 SpringApplication.run(CustomerSchoolApplication.class, args);
 	}
 }
