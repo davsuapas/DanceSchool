d6b0ca046c9ef40f6b5c6747a1a092b641b6adf1
David suarez <dav.sua.pas@gmail.com>
2017-05-22 10:31:24 +0200

    Create home page

---
+++ WebSchool/src/main/java/com/elipcero/schoolweb/home/HomeController.java
@@ -0,0 +1,13 @@
+package com.elipcero.schoolweb.home;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class HomeController {
+
+	@RequestMapping("/")
+	public String home() {
+		return "home/home";
+	}
+}
