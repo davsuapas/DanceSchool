74154709810cbc3bb0d4dae45e25a1d1353b42dc
dav.sua.pas@gmail.com <dav.sua.pas@gmail.com>
2018-05-07 14:14:25 +0200

    Configure Customer for configuration server

--- WebSchool/src/main/java/com/elipcero/schoolweb/customer/services/CustomerResource.java
+++ WebSchool/src/main/java/com/elipcero/schoolweb/customer/services/CustomerResource.java
@@ -7,7 +7,7 @@ import org.springframework.hateoas.Resource;
 import org.springframework.hateoas.Resources;
 import org.springframework.web.bind.annotation.*;
 
-@FeignClient(name = "customer-school")
+@FeignClient(name = "customerschool")
 public interface CustomerResource {
 	
     @GetMapping(value = "customers/search/secondName?secondName={secondName}&projection=descriptor")
