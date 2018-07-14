7b06a25f3ca7af1c3aeabadaf8f9adb179c50f30
dav.sua.pas@gmail.com <dav.sua.pas@gmail.com>
2018-07-14 18:39:39 +0200

    Addinglogout

---
+++ WebSchool/src/main/java/com/elipcero/schoolweb/Configuration/WebSecurityConfiguration.java
@@ -0,0 +1,28 @@
+package com.elipcero.schoolweb.Configuration;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
+
+@Configuration
+@EnableWebSecurity
+public class WebSecurityConfiguration extends WebSecurityConfigurerAdapter {
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http
+                .authorizeRequests()
+                .antMatchers("/logout").permitAll()
+                .anyRequest().authenticated()
+                .and()
+                    .oauth2Login()
+                .and()
+                    .logout()
+                    .logoutRequestMatcher(new AntPathRequestMatcher("/logout")) // For demos.
+                    .clearAuthentication(true)
+                    .invalidateHttpSession(true)
+                    .deleteCookies("auth_code", "JSESSIONID");
+    }
+}
