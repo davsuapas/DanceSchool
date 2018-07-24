8571a5a4704c3abda829527badbb202b2cc1b078
dav.sua.pas@gmail.com <dav.sua.pas@gmail.com>
2018-07-24 14:40:59 +0200

    Change namespace configuration of web

--- WebSchool/src/main/java/com/elipcero/schoolweb/configuration/WebSecurityConfiguration.java
+++ WebSchool/src/main/java/com/elipcero/schoolweb/configuration/WebSecurityConfiguration.java
@@ -1,4 +1,4 @@
-package com.elipcero.schoolweb.Configuration;
+package com.elipcero.schoolweb.configuration;
 
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
